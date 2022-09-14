900e822f3313037d18bdfb85f2806116249726e1
Anton Prosekin <ayuprosekin@yandex.ru>
2022-09-14 10:05:51 +0800

    Add junit tests

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.FilmAlreadyExistException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -10,22 +11,26 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 
+@Slf4j
 @RestController("/films")
 public class FilmController {
 
     private final HashMap<Integer, Film> films = new HashMap<>();
 
-    @GetMapping
+    @GetMapping(value = "/films")
     public Collection<Film> findAll() {
         return films.values();
     }
 
-    @PostMapping
+    @PostMapping(value = "/films")
     public Film create(@RequestBody Film film) {
+
+        log.info("Создание (post) записи для фильма {}", film.getName());
 
         filmValidator(film);
 
         if (films.containsKey(film.getId())) {
+            log.info("Film with id = {} already exists.", film.getId());
             throw new FilmAlreadyExistException("Film with id = " + film.getId() + " already exists.");
         }
         films.put(film.getId(), film);
@@ -35,26 +40,33 @@ public class FilmController {
     @PutMapping
     public Film put(@RequestBody Film film) {
 
+        log.info("Редактирование (put) записи для фильма {}", film.getName());
+
         filmValidator(film);
         films.put(film.getId(), film);
         return film;
     }
 
     private void filmValidator(Film film) {
+
         if (film.getName().isBlank()) {
+            log.info("Валидация не пройдена: пустое назание");
             throw new ValidationException("Название не может быть пустым.");
         }
 
         if (film.getDescription().length() > 200) {
+            log.info("Валидация не пройдена: описание длиннее 200 символов");
             throw new ValidationException("Максимальная длина описание 200 символов.");
         }
 
         LocalDate firstRelease = LocalDate.of(1895,12,28);
         if (film.getReleaseDate().isBefore(firstRelease)) {
-            throw new ValidationException("Выход фильма не может быть раньше " + firstRelease);
+            log.info("Валидация не пройдена: дата релиза раньше 28.12.1895");
+            throw new ValidationException("Выход фильма не может быть раньше 28.12.1895.");
         }
 
         if (film.getDuration().isNegative()) {
+            log.info("Валидация не пройдена: продолжительность фильма отрицательна");
             throw new ValidationException("Продолжительность фильма должна быть положительной.");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UserAlreadyExistException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 
+@Slf4j
 @RestController("/users")
 public class UserController {
     private final HashMap<Integer, User> users = new HashMap<>();
@@ -19,8 +21,10 @@ public class UserController {
         return users.values();
     }
 
-    @PostMapping
+    @PostMapping(value = "/users")
     public User create(@RequestBody User user) {
+
+        log.info("Создание (post) записи для пользователя {}", user.getName());
 
         userValidator(user);
 
@@ -32,9 +36,10 @@ public class UserController {
         return user;
     }
 
-    @PutMapping
+    @PutMapping(value = "/users")
     public User put(@RequestBody User user) {
 
+        log.info("Редактирование (put) записи для пользователя {}", user.getName());
         userValidator(user);
         users.put(user.getId(), user);
 
@@ -43,18 +48,22 @@ public class UserController {
 
     private void userValidator(User user) {
         if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.info("Валидация не пройдена: email пуст или не содержит @");
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать @");
         }
 
         if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.info("Валидация не пройдена: логин пуст или содержит пробелы");
             throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
         }
 
         if (user.getName().isBlank()) {
+            log.info("Имя пользователя пустое: будет использоваться логин");
             user.setName(user.getLogin());
         }
         LocalDate firstRelease = LocalDate.now();
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("Валидация не пройдена: дата рождения не может быть в будущем");
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
 
