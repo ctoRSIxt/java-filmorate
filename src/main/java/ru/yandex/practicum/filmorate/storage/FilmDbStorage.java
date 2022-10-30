83eb78cd2457334bf0123558f4ee12155193d938
Anton Prosekin <ayuprosekin@yandex.ru>
2022-10-31 00:47:06 +0800

    Small fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -200,6 +200,10 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film create(Film film) {
+        
+        if (film.getMpa() == null) {
+            throw new ValidationException("mpa рейтинг не может быть пустым");
+        };
 
         validateFilm(film);
 
